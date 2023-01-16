31e40389ce2558b18952788599b745943f58dea0
RedSTARO <822858602@qq.com>
2023-01-16 20:34:04 +0800

    Revert "devicecode/soccode: Build vendor variant for libstdc++"
    This reverts commit 61ed49e27bbf327fa72bacad6c003120d3c1a646.

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -90,8 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     camera.device@3.2-impl:32 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    libstdc++.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # CONFIGSTORE
 PRODUCT_PACKAGES += \
